ed70dde4bd3e62e5e91ceaa785b64dc4b9cdd954
Thanusha Jayasuriya <59741871+thanushanj@users.noreply.github.com>
2023-10-08 20:43:15 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+def main():
+    def firstFunction():
+        print("Hello World!")
+        
+    def secondFunction():
+        num1 = int(input("Enter Number 1: "))
+        num2 = int(input("Enter Number 2: "))
+        
+        addVal = num1 + num2
+        multiVal = num1 * num2
+        
+        print("Addition =", addVal)
+        print("Multiplication =", multiVal)
+        
+    firstFunction()
+    secondFunction()
+
+if __name__ == "__main__":
+    main()
