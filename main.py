3649dfeb5a4cb4ca66af3a1c3a57d0608e207df1
Madhuka Dilshan <madhukad01@gmail.com>
2023-10-08 22:44:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
     def firstFunction():
-        print("Hello World!")
+        print("Welcome to our YouTube Channel")
         
     def secondFunction():
         num1 = int(input("Enter Number 1: "))
