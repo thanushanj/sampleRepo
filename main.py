1f93d3d9df0bdd75113093b8121426c4338e97fe
Madhuka Dilshan <madhukad01@gmail.com>
2023-10-15 12:00:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 def main():
     def firstFunction():
-        print("Hello World!")
+        print("Hello GitHub!")
         
     def secondFunction():
         num1 = int(input("Enter Number 1: "))
         num2 = int(input("Enter Number 2: "))
         
         addVal = num1 + num2
+        subVal = num1 - num2
         multiVal = num1 * num2
+        divVal = num1 / num2
         
         print("Addition =", addVal)
+        print("Subtraction =", subVal)
         print("Multiplication =", multiVal)
+        print("Division =", divVal)
         
     firstFunction()
     secondFunction()
